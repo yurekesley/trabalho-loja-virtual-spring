3df809814701f3eda50de1ad0dc874f047f09879
MDIAS\ter01282 <yurekesley@gmail.com>
2019-05-14 18:00:26 -0300

    configurando ambiente de testes

--- qualquer-coisa-app/src/test/java/br/com/yurekesley/qualquercoisaapp/QualquerCoisaAppApplicationTests.java
+++ qualquer-coisa-app/src/test/java/br/com/yurekesley/qualquercoisaapp/QualquerCoisaAppApplicationTests.java
@@ -4,12 +4,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.yurekesley.qualquercoisaapp.service.ClienteService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest("application.test.properties")
+@TestPropertySource(locations = "classpath:application.test.properties")
+@SqlGroup({
+    @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:beforeTestRun.sql"),
+    @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:afterTestRun.sql") })
 public class QualquerCoisaAppApplicationTests {
 
 	@Autowired
